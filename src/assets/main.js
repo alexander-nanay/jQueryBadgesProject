a5597f85962650a5f468016612fe2b88f63777a0
alexander-nanay <alexander.nanay@hotmail.com>
2017-04-22 17:59:33 +0300

    Display badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
+  var processData = function(data) {
+    console.log(data.courses.completed);
+    var badges = $("#badges");
+    data.courses.completed.forEach(function(course) {
+      badges.append(
+        `<div class='course'>
+          <h3>${course.title}</h3>
+          <img src=${course.badge}></img>
+          <a href=${course.url} target='_blank' class='btn btn-primary'>See Course</a>
+        </div>`);
+    });
+  };
 
-  // your code will go here
-
+  $.ajax({
+    url: "https://www.codeschool.com/users/alexnanay.json",
+    dataType: "jsonp",
+    success: processData
+  })
 });
